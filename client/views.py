24147431b7c27f508ff88e62d93e28b82d95868a
lenixbyte <priyankabhardwaj065@gmail.com>
2023-03-06 00:26:00 +0530

    removed redundant imports

--- client/views.py
+++ client/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Post
 from django.views.decorators.csrf import csrf_exempt
-import logging
 
 @csrf_exempt
 def Review(request, post_id=None):
